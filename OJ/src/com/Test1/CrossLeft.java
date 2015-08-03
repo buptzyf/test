a0a3272e077c10e8fc3a853c2ec3657b386944b4
buptzyf <bupt_zyf@163.com>
2015-08-03 10:48:25 +0800

    first commit

---
+++ OJ/src/com/Test1/CrossLeft.java
@@ -0,0 +1,10 @@
+package com.Test1;
+
+public class CrossLeft {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
+                                                                                                                           
